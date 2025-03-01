d87ab1ced06269e03a9b50995a4136c18b51e54d
Tamim <jobaertamim7@gmail.com>
2025-03-02 05:24:44 +0600

    all done except blog page

---
+++ scripts/test.js
@@ -0,0 +1,51 @@
+const activityCardContainer = document.getElementById("activity-card-container");
+
+const taskNumInfo = {};
+taskNumInfo["assigned"] = parseInt(document.getElementById("assigned-task-num").innerText);
+taskNumInfo["completed"] = parseInt(document.getElementById("completed-task").innerText);
+
+activityCardContainer.addEventListener("click",
+    function(event){
+        event.preventDefault();
+        const element = event.target;
+        if(element.tagName === "BUTTON"){
+            alert("Board updated successfully !")
+            console.log(getTitleByElement(element));
+            element.disabled = true;
+            updateTaskNum(taskNumInfo,"completed")
+            if(taskNumInfo.assigned == 0){
+                alert("Congrats ! You have completed all the current tasks.")
+            }
+            activityLogWriter(getTitleByElement(element));
+        }
+        
+    }
+)
+
+
+document.getElementById("clear-btn").addEventListener("click",
+    function (event){
+        event.preventDefault();
+        activityLogDeleter();
+    }
+)
+
+document.getElementById("theme-btn").addEventListener("click",
+    function (event){
+        bgColorChange();
+    }
+)
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const date = currentDateGenerator();
+    document.getElementById("day").innerText = date.day;
+    document.getElementById("else-date").innerText = date.month + " " + date.date + " " + date.year;
+});
+
+
+document.getElementById("discover-btn").addEventListener("click",
+    function(){
+        
+    }
+)
---
+++ scripts/utilities.js
@@ -0,0 +1,75 @@
+function getTitleByElement(element){
+    const parentElement = element.parentElement;
+    const discriptionElement = parentElement.previousElementSibling;
+    const titleElement = discriptionElement.previousElementSibling;
+
+    return titleElement.innerText;
+}
+
+function activityLogWriter(title){
+    const newLog = document.createElement("div");
+    const classStrings = "text-sm rounded-lg bg-bg-primary px-4 pb-4 my-4".split(" ");
+    
+    for (const classString of classStrings){
+        newLog.classList.add(classString);
+    }
+
+    const currentTime = currentTimeGenerator();
+    
+    newLog.innerText = `
+    You have completed task ${title} at ${currentTime}
+    `
+
+    const activityLog = document.getElementById("activity-log");
+    
+    activityLog.appendChild(newLog);
+    
+}
+
+function activityLogDeleter(){
+    const activityLog = document.getElementById("activity-log");
+
+    activityLog.innerHTML = "";
+}
+
+function bgColorChange(){
+    const max = 255;
+    const min = 230;
+    const red = Math.floor(Math.random()*(max-min)) + min;
+    const green = Math.floor(Math.random()*(max-min)) + min;
+    const blue = Math.floor(Math.random()*(max-min)) + min;
+
+    const htmlElement = document.getElementsByTagName("html")[0];
+    htmlElement.style.backgroundColor = `rgb(${red}, ${green} , ${blue})`
+}
+function currentTimeGenerator(){
+    const date = new Date();
+    const time = date.toLocaleTimeString("en-US",{
+        hour12 : true,
+    })
+
+
+    return time;
+}
+
+function currentDateGenerator(){
+    const date = new Date();
+    const convertedDate = date.toDateString().split(" ");
+    const dateObj = {};
+    dateObj.day = convertedDate[0];
+    dateObj.month = convertedDate[1];
+    dateObj.date = convertedDate[2];
+    dateObj.year = convertedDate[3];
+
+    return dateObj;
+}
+
+function updateTaskNum(taskObj,type){
+    if(type === "completed"){
+        taskObj.assigned -= 1;
+        taskObj.completed += 1;
+
+        document.getElementById("assigned-task-num").innerText = taskObj.assigned;
+        document.getElementById("completed-task").innerText = taskObj.completed;
+    }
+}
