4b40f3d263c931a547be54a0ea8c13698d3d3b47
Tamim <jobaertamim7@gmail.com>
2025-03-02 21:48:38 +0600

    blog.html 1st completation

---
+++ scripts/blog.js
@@ -0,0 +1,8 @@
+document.getElementById("back-btn").addEventListener("click",
+    function(event){
+        event.preventDefault();
+        window.location.href = "../index.html"
+    }
+)
+
+document.createElement("p").appendChild();
